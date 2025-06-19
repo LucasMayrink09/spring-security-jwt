69c9ad29f2c59f7438c67e1c4b59afbc040f0349
Lucas Mayrink Oliveira <lucas.mayrink@zerohora.com.br>
2025-06-19 17:58:05 -0300

    add configuration classes

--- src/main/java/lucas/mayrink/springsecurity/config/AdminUserConfig.java
+++ src/main/java/lucas/mayrink/springsecurity/config/AdminUserConfig.java
@@ -1,4 +1,50 @@
 package lucas.mayrink.springsecurity.config;
 
-public class AdminUserConfig {
+import jakarta.transaction.Transactional;
+import lucas.mayrink.springsecurity.entities.Role;
+import lucas.mayrink.springsecurity.entities.User;
+import lucas.mayrink.springsecurity.repository.RoleRepository;
+import lucas.mayrink.springsecurity.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.Set;
+
+@Configuration
+public class AdminUserConfig implements CommandLineRunner {
+
+    private RoleRepository roleRepository;
+    private UserRepository userRepository;
+    private BCryptPasswordEncoder passwordEncoder;
+
+    public AdminUserConfig(RoleRepository roleRepository,
+                           UserRepository userRepository,
+                           BCryptPasswordEncoder passwordEncoder) {
+        this.roleRepository = roleRepository;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    @Transactional
+    public void run(String... args) throws Exception {
+
+        var roleAdmin = roleRepository.findByName(Role.Values.ADMIN.name());
+
+        var userAdmin = userRepository.findByUsername("admin");
+
+        userAdmin.ifPresentOrElse(
+                user -> {
+                    System.out.println("admin ja existe");
+                },
+                () -> {
+                    var user = new User();
+                    user.setUsername("admin");
+                    user.setPassword(passwordEncoder.encode("123"));
+                    user.setRoles(Set.of(roleAdmin));
+                    userRepository.save(user);
+                }
+        );
+    }
 }
