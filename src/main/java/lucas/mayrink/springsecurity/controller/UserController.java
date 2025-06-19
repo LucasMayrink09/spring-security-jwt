4e14889fef5063ea690af10adc3aa6d106322752
Lucas Mayrink Oliveira <lucas.mayrink@zerohora.com.br>
2025-06-19 17:48:00 -0300

    create entity classes

--- src/main/java/lucas/mayrink/springsecurity/controller/UserController.java
+++ src/main/java/lucas/mayrink/springsecurity/controller/UserController.java
@@ -1,4 +1,28 @@
 package lucas.mayrink.springsecurity.controller;
 
+import jakarta.transaction.Transactional;
+import lucas.mayrink.springsecurity.repository.RoleRepository;
+import lucas.mayrink.springsecurity.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    public UserController(UserRepository userRepository,
+                          RoleRepository roleRepository,
+                          BCryptPasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Transactional
+    @PostMapping("/users")
+    public ResponseEntity
 }
--- src/main/java/lucas/mayrink/springsecurity/entities/Role.java
+++ src/main/java/lucas/mayrink/springsecurity/entities/Role.java
@@ -7,8 +7,40 @@ import jakarta.persistence.*;
 public class Role {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
     private Long roleId;
-
     private String name;
+
+    public Long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public enum Values {
+
+        ADMIN(1L);
+        BASIC(2l);
+
+        long roleId;
+
+        Values(long roleId) {
+            this.roleId = roleId;
+        }
+
+        public Long getRoleId() {
+            return roleId;
+        }
+    }
 }
